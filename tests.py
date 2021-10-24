a1f690765310a39bae0418e332fbc081e50f3192
soumik12345 <19soumik.rakshit96@gmail.com>
2021-10-24 19:35:24 +0530

    Updated tests

--- tests.py
+++ tests.py
@@ -11,55 +11,55 @@ from point_seg import (
 from point_seg import models
 
 
-# DATASET_URL = "https://github.com/soumik12345/point-cloud-segmentation/releases/download/v0.1/shapenet.zip"
+DATASET_URL = "https://github.com/soumik12345/point-cloud-segmentation/releases/download/v0.1/shapenet.zip"
 
 
-# class DataLoaderTester(unittest.TestCase):
-#     def __init__(self, *args, **kwargs) -> None:
-#         super().__init__(*args, **kwargs)
-#         tf.keras.utils.get_file(
-#             fname="shapenet.zip",
-#             origin=DATASET_URL,
-#             cache_subdir="datasets",
-#             hash_algorithm="auto",
-#             extract=True,
-#             archive_format="auto",
-#             cache_dir="datasets",
-#         )
-#         self.in_memory_data_loader = ShapeNetCoreLoaderInMemory(
-#             object_category="Airplane", viz_samples=100
-#         )
-#         self.in_memory_data_loader.load_data()
-#         self.e2e_data_loader = ShapeNetCoreLoader(object_category="Airplane")
+class DataLoaderTester(unittest.TestCase):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        tf.keras.utils.get_file(
+            fname="shapenet.zip",
+            origin=DATASET_URL,
+            cache_subdir="datasets",
+            hash_algorithm="auto",
+            extract=True,
+            archive_format="auto",
+            cache_dir="datasets",
+        )
+        self.in_memory_data_loader = ShapeNetCoreLoaderInMemory(
+            object_category="Airplane", viz_samples=100
+        )
+        self.in_memory_data_loader.load_data()
+        self.e2e_data_loader = ShapeNetCoreLoader(object_category="Airplane")
 
-#     def test_in_memory_data_loader(self):
-#         train_dataset, val_dataset = self.in_memory_data_loader.get_datasets()
-#         x, y = next(iter(train_dataset))
-#         assert x.shape == (16, 1024, 3)
-#         assert y.shape == (16, 1024, 5)
-#         x, y = next(iter(val_dataset))
-#         assert x.shape == (16, 1024, 3)
-#         assert y.shape == (16, 1024, 5)
+    def test_in_memory_data_loader(self):
+        train_dataset, val_dataset = self.in_memory_data_loader.get_datasets()
+        x, y = next(iter(train_dataset))
+        assert x.shape == (16, 1024, 3)
+        assert y.shape == (16, 1024, 5)
+        x, y = next(iter(val_dataset))
+        assert x.shape == (16, 1024, 3)
+        assert y.shape == (16, 1024, 5)
 
-#     def test_e2e_data_loader(self):
-#         train_dataset, val_dataset = self.e2e_data_loader.get_datasets()
-#         x, y = next(iter(train_dataset))
-#         assert x.shape == (16, 1024, 3)
-#         assert y.shape == (16, 1024, 5)
-#         x, y = next(iter(val_dataset))
-#         assert x.shape == (16, 1024, 3)
-#         assert y.shape == (16, 1024, 5)
+    def test_e2e_data_loader(self):
+        train_dataset, val_dataset = self.e2e_data_loader.get_datasets()
+        x, y = next(iter(train_dataset))
+        assert x.shape == (16, 1024, 3)
+        assert y.shape == (16, 1024, 5)
+        x, y = next(iter(val_dataset))
+        assert x.shape == (16, 1024, 3)
+        assert y.shape == (16, 1024, 5)
 
 
-# class ShapeSegmentModelTester(unittest.TestCase):
-#     def __init__(self, *args, **kwargs) -> None:
-#         super().__init__(*args, **kwargs)
-#         self.shapenet_model = models.get_shape_segmentation_model(2048, 5)
+class ShapeSegmentModelTester(unittest.TestCase):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.shapenet_model = models.get_shape_segmentation_model(2048, 5)
 
-#     def test_model_output_shape(self):
-#         random_inputs = tf.random.normal((16, 2048, 3))
-#         random_predictions = self.shapenet_model.predict(random_inputs)
-#         assert random_predictions.shape == (16, 2048, 5)
+    def test_model_output_shape(self):
+        random_inputs = tf.random.normal((16, 2048, 3))
+        random_predictions = self.shapenet_model.predict(random_inputs)
+        assert random_predictions.shape == (16, 2048, 5)
 
 
 class TFRecordCreationTester(unittest.TestCase):
