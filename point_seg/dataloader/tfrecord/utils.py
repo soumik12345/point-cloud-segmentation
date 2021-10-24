8775c992ba729743ef5802c8fdfa8eaddba46552
soumik12345 <19soumik.rakshit96@gmail.com>
2021-10-24 22:08:42 +0530

    updated tfrecord creator

--- point_seg/dataloader/tfrecord/utils.py
+++ point_seg/dataloader/tfrecord/utils.py
@@ -7,6 +7,13 @@ def get_float_feature(value):
     return feature(value)
 
 
+def bytes_feature(value):
+    """Returns a bytes_list from a string / byte."""
+    if isinstance(value, type(tf.constant(0))): # if value ist tensor
+        value = value.numpy() # get value of tensor
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
 def split_list(given_list: List, chunk_size: int) -> List:
     return [
         given_list[offs : offs + chunk_size]
@@ -16,6 +23,8 @@ def split_list(given_list: List, chunk_size: int) -> List:
 
 def create_example(point_cloud, label_cloud) -> Dict:
     return {
-        "point_cloud": get_float_feature(point_cloud.reshape(-1)),
-        "label_cloud": get_float_feature(label_cloud.reshape(-1)),
+        # "point_cloud": get_float_feature(point_cloud.reshape(-1)),
+        # "label_cloud": get_float_feature(label_cloud.reshape(-1)),
+        "point_cloud": bytes_feature(tf.io.serialize_tensor(point_cloud)),
+        "label_cloud": bytes_feature(tf.io.serialize_tensor(label_cloud)),
     }
