c1d29e243e98f31013643c007193fc048e63761b
soumik12345 <19soumik.rakshit96@gmail.com>
2021-10-27 12:11:17 +0530

    updated tests

--- tests.py
+++ tests.py
@@ -93,12 +93,12 @@ class TFRecordTester(unittest.TestCase):
         assert len(train_tfrecord_files) == 1
         assert len(val_tfrecord_files) == 1
 
-    def test_tfrecord_loader(self):
-        loader = TFRecordLoader(self.tfrecord_dir, self.object_category)
-        train_ds, val_ds = loader.get_datasets(batch_size=self.batch_size)
-        x, y = next(iter(train_ds))
-        assert x.shape == (16, 1024, 3)
-        assert y.shape == (16, 1024, 5)
-        x, y = next(iter(val_ds))
-        assert x.shape == (16, 1024, 3)
-        assert y.shape == (16, 1024, 5)
+    # def test_tfrecord_loader(self):
+    #     loader = TFRecordLoader(self.tfrecord_dir, self.object_category)
+    #     train_ds, val_ds = loader.get_datasets(batch_size=self.batch_size)
+    #     x, y = next(iter(train_ds))
+    #     assert x.shape == (16, 1024, 3)
+    #     assert y.shape == (16, 1024, 5)
+    #     x, y = next(iter(val_ds))
+    #     assert x.shape == (16, 1024, 3)
+    #     assert y.shape == (16, 1024, 5)
